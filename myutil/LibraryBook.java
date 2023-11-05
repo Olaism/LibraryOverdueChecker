ca19f2a110bff63692c0d406a0a54e87b99aa5c1
olaism <olaism01@gmail.com>
2023-11-05 12:44:42 -0800

    Add computCharge method in LibraryBook class

--- myutil/LibraryBook.java
+++ myutil/LibraryBook.java
@@ -13,6 +13,7 @@ class LibraryBook {
     private static final double CHARGE_PER_DAY = 0.50;
     private static final double MAX_CHARGE = 50.00;
     private static final String DEFAULT_TITLE = "Title unknown";
+    private static final double MILLISEC_TO_DAY = 1.0 / 1000 / 60 / 60 / 24;
     private GregorianCalendar dueDate;
     private String title;
     private double chargePerDay;
@@ -62,6 +63,21 @@ class LibraryBook {
     }
     public void setTitle(String title) {
         this.title = title;
+    }
+    public double computeCharge(GregorianCalendar returnDate) {
+        double charge = 0.0;
+
+        long dueTime = dueDate.getTimeInMillis();
+        long returnTime = returnDate.getTimeInMillis();
+
+        long diff = returnTime - dueTime;
+        if (diff > 0) {
+            charge = chargePerDay * diff * MILLISEC_TO_DAY;
+            if (charge > maximumCharge) {
+                charge = maximumCharge;
+            }
+        }
+        return charge;
     }
     public String toString() {
         return String.format("%-30s $%5.2f $%7.2f %4$tm/%4$td/%4$ty", 
